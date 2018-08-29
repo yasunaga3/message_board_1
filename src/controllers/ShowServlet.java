243ccda72dc1b26b971b66e74a306c62784f5c46
masahiro yasunaga <yasunaga37@gmail.com>
2018-08-30 08:52:47 +0900

    Add ShowServlet

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -15,34 +14,30 @@ import models.Message;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class ShowServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public IndexServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		System.out.println(" IndexServlet ");
-
-		EntityManager em = DBUtil.createEntityManager();
-		List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
-		em.close();
-
-	    request.setAttribute("messages", messages);
-	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
-	    rd.forward(request, response);
+        EntityManager em = DBUtil.createEntityManager();
+        Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
+        em.close();
+        request.setAttribute("message", m);
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/show.jsp");
+        rd.forward(request, response);
 	}
 
 }
